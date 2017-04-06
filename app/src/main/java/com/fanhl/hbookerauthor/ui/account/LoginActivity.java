cdb3e655630dffeaf84ff847e07d93f3f6281e00
fanhl <fanhonglin@klicen.com>
2017-04-06 20:22:02 +0800

    Init Commit

---
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/account/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.fanhl.hbookerauthor.ui.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fanhl.hbookerauthor.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
