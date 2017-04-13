c4b23c8f59ecc45023cb1924c4540cc8fc35e66e
fanhl <fanhonglin@klicen.com>
2017-04-13 18:53:12 +0800

    palette

--- app/src/main/java/com/fanhl/hbookerauthor/ui/main/BooksManagerAdapter.java
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/main/BooksManagerAdapter.java
@@ -1,12 +1,19 @@
 package com.fanhl.hbookerauthor.ui.main;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.v7.graphics.Palette;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.fanhl.hbookerauthor.R;
 import com.fanhl.hbookerauthor.data.Book;
 import com.fanhl.hbookerauthor.ui.common.ListAdapter;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 /**
  * Created by fanhl on 2017/4/11.
@@ -21,13 +28,49 @@ class BooksManagerAdapter extends ListAdapter<BooksManagerAdapter.ViewHolder, Bo
 
     public class ViewHolder extends ListAdapter.ViewHolder {
 
+        private final LinearLayout container;
+        private final ImageView coverImg;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            container = ((LinearLayout) itemView.findViewById(R.id.container));
+            coverImg = ((ImageView) itemView.findViewById(R.id.coverImg));
+
+//            Palette.from(coverImg.getDrawingCache()).generate(palette -> {
+//                container.setBackgroundColor(palette.getLightVibrantSwatch().getRgb());
+//            });
         }
 
         @Override
         public void bind(Object data) {
             super.bind(data);
+            Book book = (Book) data;
+
+            // https://novel-cdn.kuangxiangit.com/images/default.jpg
+            Picasso.with(coverImg.getContext())
+                    .load("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2971967025,2497343324&fm=80&w=179&h=119&img.JPEG")
+                    .into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            coverImg.setImageBitmap(bitmap);
+                            Palette.from(bitmap).generate(palette -> {
+                                if (palette.getVibrantSwatch() != null) {
+                                    container.setBackgroundColor(palette.getVibrantSwatch().getRgb());
+                                }
+                            });
+
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                        }
+                    });
         }
     }
 }
