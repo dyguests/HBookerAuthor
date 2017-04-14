cec3a4f255e6fe424273bbf1bfa1018753dd50ed
fanhl <fanhonglin@klicen.com>
2017-04-14 18:13:59 +0800

    BookOperationsDialogFragment goto web

--- app/src/main/java/com/fanhl/hbookerauthor/ui/main/widget/BookOperationsDialogFragment.java
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/main/widget/BookOperationsDialogFragment.java
@@ -1,5 +1,7 @@
 package com.fanhl.hbookerauthor.ui.main.widget;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.fanhl.hbookerauthor.R;
 import com.fanhl.hbookerauthor.data.Book;
@@ -18,14 +21,16 @@ import com.fanhl.hbookerauthor.data.Book;
 public class BookOperationsDialogFragment extends BottomSheetDialogFragment {
     public static final String TAG = BookOperationsDialogFragment.class.getSimpleName();
 
-    private BookOperationsDialogFragment data;
+    private Button detailBtn;
+
+    private Book data;
 
     public static BookOperationsDialogFragment newInstance(@NonNull Book data) {
         Bundle args = new Bundle();
 
         BookOperationsDialogFragment fragment = new BookOperationsDialogFragment();
         fragment.setArguments(args);
-        fragment.data = fragment;
+        fragment.data = data;
         return fragment;
     }
 
@@ -33,8 +38,18 @@ public class BookOperationsDialogFragment extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_book_operations, container, false);
+        assignViews(view);
         return view;
     }
 
-    //http://www.hbooker.com/book/book_detail/{bookId}
+    private void assignViews(View view) {
+        this.detailBtn = (Button) view.findViewById(R.id.detailBtn);
+
+        detailBtn.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.hbooker.com/book/book_detail?book_id=" + data.getId()));
+            startActivity(browserIntent);
+
+            dismiss();
+        });
+    }
 }
