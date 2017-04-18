4f259cb6c0d89565901ca36508c91ee90a09c841
fanhl <fanhonglin@klicen.com>
2017-04-18 10:18:22 +0800

    new BookActivity include fragment

---
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/book/Book2Activity.java
@@ -0,0 +1,148 @@
+package com.fanhl.hbookerauthor.ui.book;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.fanhl.hbookerauthor.R;
+import com.fanhl.hbookerauthor.data.Book;
+import com.fanhl.hbookerauthor.data.Tag;
+import com.fanhl.hbookerauthor.io.jsoup.response.BookSettingResponse;
+import com.fanhl.hbookerauthor.ui.book.adapter.TagAdapter;
+import com.fanhl.hbookerauthor.ui.book.widget.TagPickerDialogFragment;
+import com.fanhl.hbookerauthor.ui.common.BaseActivity;
+import com.fanhl.hbookerauthor.util.Log;
+import com.google.android.flexbox.FlexboxLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * 作品页面
+ */
+public class Book2Activity extends BaseActivity {
+    public static final String TAG = Book2Activity.class.getSimpleName();
+    public static final String EXTRA_DATA = "EXTRA_DATA";
+
+    private RecyclerView tagRecyclerView;
+    private TagAdapter tagAdapter;
+    private BookSettingResponse bookSettingResponse;
+
+    private Book data;
+
+    public static void launch(Context context, Book data) {
+        Intent intent = new Intent(context, Book2Activity.class);
+        intent.putExtra(EXTRA_DATA, data);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book2);
+
+        data = getIntent().getParcelableExtra(EXTRA_DATA);
+
+        assignViews();
+        initData();
+        refreshData();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void assignViews() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        tagRecyclerView = ((RecyclerView) findViewById(R.id.tagRecyclerView));
+        tagRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        tagRecyclerView.setLayoutManager(new FlexboxLayoutManager());
+    }
+
+    private void initData() {
+        getSupportActionBar().setTitle(data.getTitle());
+
+        tagAdapter = new TagAdapter();
+        tagRecyclerView.setAdapter(tagAdapter);
+
+        tagAdapter.setOnItemClickListener((position, holder) -> {
+            TagAdapter.ViewHolder tagHolder = (TagAdapter.ViewHolder) holder;
+            if (tagHolder instanceof TagAdapter.NormalViewHolder) {
+                Tag tag = (Tag) tagHolder.getData();
+                // FIXME: 2017/4/17
+            } else if (tagHolder instanceof TagAdapter.AddViewHolder) {
+                showAddTagDialog();
+            }
+        });
+    }
+
+    private void refreshData() {
+        getApp().getClient().getBookService()
+                .book_info(data.getId())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<BookSettingResponse>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(BookSettingResponse bookSettingResponse) {
+                        bindData(bookSettingResponse);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError 获取bookSettingResponse失败", e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
+
+        if (false) {
+            List<Tag> list = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                list.add(new Tag("Tag" + i));
+            }
+            tagAdapter.replaceItems(list);
+        }
+    }
+
+    private void bindData(BookSettingResponse bookSettingResponse) {
+        this.bookSettingResponse = bookSettingResponse;
+        tagAdapter.replaceItems(bookSettingResponse.getMyTags());
+    }
+
+    private void showAddTagDialog() {
+        TagPickerDialogFragment.newInstance(bookSettingResponse.getTags(), bookSettingResponse.getMyTags())
+                .show(getSupportFragmentManager(), TagPickerDialogFragment.TAG);
+    }
+}
--- app/src/main/java/com/fanhl/hbookerauthor/ui/book/BookActivity.java
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/book/BookActivity.java
@@ -2,49 +2,54 @@ package com.fanhl.hbookerauthor.ui.book;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 import com.fanhl.hbookerauthor.R;
 import com.fanhl.hbookerauthor.data.Book;
-import com.fanhl.hbookerauthor.data.Tag;
-import com.fanhl.hbookerauthor.io.jsoup.response.BookSettingResponse;
-import com.fanhl.hbookerauthor.ui.book.adapter.TagAdapter;
-import com.fanhl.hbookerauthor.ui.book.widget.TagPickerDialogFragment;
-import com.fanhl.hbookerauthor.ui.common.BaseActivity;
-import com.fanhl.hbookerauthor.util.Log;
-import com.google.android.flexbox.FlexboxLayoutManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-
-/**
- * 作品页面
- */
-public class BookActivity extends BaseActivity {
-    public static final String TAG = BookActivity.class.getSimpleName();
+public class BookActivity extends AppCompatActivity {
     public static final String EXTRA_DATA = "EXTRA_DATA";
 
     private Book data;
-    private RecyclerView tagRecyclerView;
-    private TagAdapter tagAdapter;
-    private BookSettingResponse bookSettingResponse;
+    private TabLayout tabLayout;
 
     public static void launch(Context context, Book data) {
         Intent intent = new Intent(context, BookActivity.class);
         intent.putExtra(EXTRA_DATA, data);
         context.startActivity(intent);
     }
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,17 +60,6 @@ public class BookActivity extends BaseActivity {
 
         assignViews();
         initData();
-        refreshData();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void assignViews() {
@@ -73,75 +67,117 @@ public class BookActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(mViewPager);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
-
-        tagRecyclerView = ((RecyclerView) findViewById(R.id.tagRecyclerView));
-        tagRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        tagRecyclerView.setLayoutManager(new FlexboxLayoutManager());
     }
 
     private void initData() {
-        getSupportActionBar().setTitle(data.getTitle());
-
-        tagAdapter = new TagAdapter();
-        tagRecyclerView.setAdapter(tagAdapter);
-
-        tagAdapter.setOnItemClickListener((position, holder) -> {
-            TagAdapter.ViewHolder tagHolder = (TagAdapter.ViewHolder) holder;
-            if (tagHolder instanceof TagAdapter.NormalViewHolder) {
-                Tag tag = (Tag) tagHolder.getData();
-                // FIXME: 2017/4/17
-            } else if (tagHolder instanceof TagAdapter.AddViewHolder) {
-                showAddTagDialog();
-            }
-        });
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
     }
 
-    private void refreshData() {
-        getApp().getClient().getBookService()
-                .book_info(data.getId())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<BookSettingResponse>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
 
-                    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book, menu);
+        return true;
+    }
 
-                    @Override
-                    public void onNext(BookSettingResponse bookSettingResponse) {
-                        bindData(bookSettingResponse);
-                    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, "onError 获取bookSettingResponse失败", e);
-                    }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-                    @Override
-                    public void onComplete() {
-                        Log.d(TAG, "onComplete");
-                    }
-                });
+        return super.onOptionsItemSelected(item);
+    }
 
-        if (false) {
-            List<Tag> list = new ArrayList<>();
-            for (int i = 0; i < 20; i++) {
-                list.add(new Tag("Tag" + i));
-            }
-            tagAdapter.replaceItems(list);
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_book, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
         }
     }
 
-    private void bindData(BookSettingResponse bookSettingResponse) {
-        this.bookSettingResponse = bookSettingResponse;
-        tagAdapter.replaceItems(bookSettingResponse.getMyTags());
-    }
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    private void showAddTagDialog() {
-        TagPickerDialogFragment.newInstance(bookSettingResponse.getTags(), bookSettingResponse.getMyTags())
-                .show(getSupportFragmentManager(), TagPickerDialogFragment.TAG);
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+                case 3:
+                    return "SECTION 4";
+            }
+            return null;
+        }
     }
 }
