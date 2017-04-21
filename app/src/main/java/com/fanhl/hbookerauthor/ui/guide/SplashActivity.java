f8fcdb39882003c0be69b6ecd0dbe0292e155a3f
fanhl <fanhonglin@klicen.com>
2017-04-21 20:57:35 +0800

    pre fullscreen

---
+++ app/src/main/java/com/fanhl/hbookerauthor/ui/guide/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.fanhl.hbookerauthor.ui.guide;
+
+import android.os.Bundle;
+
+import com.fanhl.hbookerauthor.R;
+import com.fanhl.hbookerauthor.ui.common.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
